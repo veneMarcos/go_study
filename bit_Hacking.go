cd58020285015424e42440cf39d7b5f48a64b1c9
Marcos Venezuela de Araujo <marcos.araujo@easynvest.com.br>
2020-09-08 22:03:38 -0300

    first commit

---
+++ bit_Hacking.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+// iota consecutives numbers
+// _ number are ignored
+const (
+	// you can do it
+	// _  = iota             // 0
+	// KB = 1 << (iota * 10) // 1 << (1 * 10)
+	// MB = 1 << (iota * 10) // 1 << (2 * 10)
+	// GB = 1 << (iota * 10) // 1 << (3 * 10)
+	// TB = 1 << (iota * 10) // 1 << (4 * 10)
+
+	// or you can do like this
+	_  = iota             // 0
+	KB = 1 << (iota * 10) // 1 << (1 * 10)
+	MB
+	GB
+	TB
+)
+
+func main() {
+	fmt.Println("binary\t\t\t\tdecimal")
+	fmt.Printf("%b\t\t\t", KB)
+	fmt.Printf("%d\n", KB)
+	fmt.Printf("%b\t\t", MB)
+	fmt.Printf("%d\n", MB)
+	fmt.Printf("%b\t", GB)
+	fmt.Printf("%d\n", GB)
+	fmt.Printf("%b\t", TB)
+	fmt.Printf("%d\n", TB)
+}
