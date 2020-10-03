193bddb5d6505cb2a965df238ac03de2c6ee5fbe
venemarcos <marcosvenezuela.araujo@gmail.com>
2020-10-03 14:26:56 -0300

    exemplos assignment

--- channel/examples/ex3.go
+++ channel/examples/ex3.go
@@ -18,3 +18,12 @@ func main() {
 	fmt.Printf("channel\t%T\n", (<-chan int)(channel))
 	fmt.Printf("channel\t%T\n", (chan<- int)(channel))
 }
+
+// - Assignment/conversion:
+//     - de geral para específico
+//     - de específico para geral não
+//     - Exemplos:
+//         - geral pra específico: https://play.golang.org/p/H1uk4YGMBB
+//         - específico pra específico: https://play.golang.org/p/8JkOnEi7-a
+//         - específico pra geral: https://play.golang.org/p/4sOKuQRHq7
+//         - atribuição tipos !=: https://play.golang.org/p/bG7H6l03VQ
